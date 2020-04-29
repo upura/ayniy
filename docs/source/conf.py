649c43a171dc8d7401044660522dd1b34faa39b1
upura <upura0@gmail.com>
2020-04-29 11:51:42 +0900

    fix version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Shotaro Ishihara'
 author = 'Shotaro Ishihara'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
