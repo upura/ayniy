f6dfc8a3d6ab71d2927cb2d33b33d42c540d95b4
upura <upura0@gmail.com>
2020-05-02 13:55:59 +0900

    create start.sh

---
+++ start.sh
@@ -0,0 +1,14 @@
+mkdir output
+mkdir experiments
+
+mkdir output/importance
+mkdir output/logs
+mkdir output/model
+mkdir output/pred
+mkdir output/submissions
+
+touch output/importance/.gitkeep
+touch output/logs/.gitkeep
+touch output/model/.gitkeep
+touch output/pred/.gitkeep
+touch output/submissions/.gitkeep
