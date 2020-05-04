7d9d632a1c4cb54ac06aa0e6f6dc7ef13ab44cdf
Shotaro Ishihara <upura0@gmail.com>
2020-05-04 23:45:36 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,27 @@
-mkdir output
-mkdir experiments
+# template
+cp -r ../ayniy/ayniy/ ayniy/
+cp ../ayniy/.dockerignore .dockerignore
+cp ../ayniy/.flake8 .flake8
+cp ../ayniy/.gitignore .gitignore
+cp ../ayniy/Dockerfile Dockerfile
+cp ../ayniy/docker-compose.yml docker-compose.yml
+cp ../ayniy/requirements.txt requirements.txt
 
+# directory
+mkdir input
+mkdir experiments
+mkdir output
 mkdir output/importance
 mkdir output/logs
 mkdir output/model
 mkdir output/pred
 mkdir output/submissions
 
+# README.md and .gitkeep
+touch input/.gitkeep
 touch output/importance/.gitkeep
 touch output/logs/.gitkeep
 touch output/model/.gitkeep
 touch output/pred/.gitkeep
 touch output/submissions/.gitkeep
+touch README.md
