a44b77e160b02b92fa67275a4900715e16634b07
upura <upura0@gmail.com>
2020-06-07 01:58:28 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,70 +1,66 @@
-import os
+from codecs import open
+from os import path
+
 from setuptools import find_packages, setup
 
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
-    README = readme.read()
+def get_long_description():
+    here = path.abspath(path.dirname(__file__))
+
+    with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+    return long_description
 
 
 def get_version():
-    # type: () -> str
-
-    version_filepath = os.path.join(os.path.dirname(__file__), 'ayniy', 'version.py')
+    version_filepath = path.join(path.dirname(__file__), 'nyaggle', 'version.py')
     with open(version_filepath) as f:
         for line in f:
             if line.startswith('__version__'):
                 return line.strip().split()[-1][1:-1]
-    assert False
-
-
-def get_install_requires():
-    install_requires = [
-        'tqdm',
-        'numpy',
-        'scikit-learn',
-        'matplotlib',
-        'seaborn',
-        'pandas',
-        'joblib',
-        'fastprogress',
-        'japanize-matplotlib',
-        'neologdn',
-        'kaggler'
-    ]
-    return install_requires
-
-
-def get_extra_requires():
-    extras = {
-        'test': ['pytest', 'ipython', 'jupyter', 'notebook', 'tornado==5.1.1'],
-        'document': ['sphinx', 'sphinx_rtd_theme']
-    }
-    return extras
 
 
 setup(
-    name='ayniy',
-    version=get_version(),
-    author='upura',
+    name='Ayniy',
     packages=find_packages(),
-    include_package_data=True,
-    license='BSD License',
-    description='Ayniy is a machine learning data pipeline especially for Kaggle.',
-    long_description=README,
-    url='https://github.com/upura',
-    author_email='upura0@gmail.com',
-    install_requires=get_install_requires(),
-    tests_require=get_extra_requires()['test'],
-    extras_require=get_extra_requires(),
-    classifiers=[
-        'Environment :: CLI Environment',
-        'Framework :: scikit-learn',
-        'Framework :: scikit-learn :: X.Y',  # replace "X.Y" as appropriate
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+
+    version=get_version(),
+
+    license='MIT',
+
+    install_requires=[
+        'git+https://github.com/stanfordmlgroup/ngboost.git'
+        'ginza',
+        'japanize-matplotlib',
+        'matplotlib',
+        'numpy',
+        'pandas',
+        'optuna>=1.0.0',
+        'seaborn',
+        'sklearn',
+        'kaggler',
+        'mecab-python3',
+        'mlflow',
+        'neologdn',
+        'tqdm',
+        'transformers==2.5.1',
     ],
+
+    extras_require={
+        'tests': ['pytest>=3.6', 'pytest-remotedata>=0.3.1']
+    },
+
+    author='upura',
+    author_email='upura0@gmail.com',
+    url='https://github.com/upura/ayniy',
+    description='Ayniy is a supporting tool for machine learning competitions.',
+    long_description=get_long_description(),
+    long_description_content_type='text/markdown',
+    keywords='ayniy kaggle',
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ]
 )
