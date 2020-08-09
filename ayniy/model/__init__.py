50351a64b5e4a6c10c65cb113cbe0fd8f5ef7ef5
upura <upura0@gmail.com>
2020-08-09 14:14:57 +0900

    Update models

--- ayniy/model/__init__.py
+++ ayniy/model/__init__.py
@@ -1,16 +1,10 @@
 from ayniy.model.model_lgbm import ModelLGBM, ModelOptunaLGBM, ModelFocalLGBM
 from ayniy.model.model_cat import ModelCatRegressor, ModelCatClassifier
-from ayniy.model.model_tabnet import ModelTabNetRegressor, ModelTabNetClassifier
 from ayniy.model.model_xgb import ModelXGB
-from ayniy.model.model_ngb import ModelNgbClassifier, ModelNgbRegressor
-from ayniy.model.model_nn import ModelTNNClassifier, ModelTNNRegressor, ModelCNNClasifier, ModelRNNClasifier
 from ayniy.model.model_ridge import ModelRIDGE
 
 
 __all__ = [ModelLGBM, ModelOptunaLGBM, ModelFocalLGBM,
            ModelCatRegressor, ModelCatClassifier,
-           ModelTabNetRegressor, ModelTabNetClassifier,
            ModelXGB,
-           ModelNgbClassifier, ModelNgbRegressor,
-           ModelTNNClassifier, ModelTNNRegressor, ModelCNNClasifier, ModelRNNClasifier,
            ModelRIDGE]
