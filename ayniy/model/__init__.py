25cf9560906c1531c3dc7216675aab92dc4cb3bb
upura <upura0@gmail.com>
2021-01-06 02:57:12 +0900

    KNN

--- ayniy/model/__init__.py
+++ ayniy/model/__init__.py
@@ -2,6 +2,7 @@ from typing import Any, List
 
 from ayniy.model.model_cat import ModelCatClassifier, ModelCatRegressor
 from ayniy.model.model_lgbm import ModelFocalLGBM, ModelLGBM, ModelOptunaLGBM
+from ayniy.model.model_knn import ModelKNN
 from ayniy.model.model_nn import ModelNN
 from ayniy.model.model_ridge import ModelRIDGE
 from ayniy.model.model_svm import ModelSVM
---
+++ ayniy/model/model_knn.py
@@ -0,0 +1,36 @@
+import os
+
+import numpy as np
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+
+from ayniy.model.model import Model
+from ayniy.utils import Data
+
+
+class ModelKNN(Model):
+    def train(
+        self,
+        tr_x: pd.DataFrame,
+        tr_y: pd.DataFrame,
+        va_x: pd.DataFrame = None,
+        va_y: pd.DataFrame = None,
+        te_x: pd.DataFrame = None,
+    ) -> None:
+
+        # ハイパーパラメータの設定
+        params = dict(self.params)
+        self.model: KNeighborsClassifier = KNeighborsClassifier(**params)
+        self.model.fit(tr_x, tr_y)
+
+    def predict(self, te_x: pd.DataFrame) -> np.ndarray:
+        return self.model.predict_proba(te_x)[:, 1]
+
+    def save_model(self) -> None:
+        model_path = os.path.join("../output/model", f"{self.run_fold_name}.model")
+        os.makedirs(os.path.dirname(model_path), exist_ok=True)
+        Data.dump(self.model, model_path)
+
+    def load_model(self) -> None:
+        model_path = os.path.join("../output/model", f"{self.run_fold_name}.model")
+        self.model = Data.load(model_path)
