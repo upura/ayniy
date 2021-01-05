f15f48980430a7bd903be8cc02e3b1e6798a2bda
upura <upura0@gmail.com>
2021-01-06 03:47:02 +0900

    Update tf keras nn

--- ayniy/model/model_nn.py
+++ ayniy/model/model_nn.py
@@ -1,23 +1,14 @@
 import os
 from typing import Any, Dict
 
-from keras import backend as K
-from keras.callbacks import EarlyStopping, Callback, ModelCheckpoint
-from keras.layers.advanced_activations import PReLU
-from keras.layers.core import Dense, Dropout
-from keras.layers import Input, Embedding, Flatten, concatenate, Multiply
-from keras.layers.normalization import BatchNormalization
-from keras.models import load_model
-from keras.models import Model as kerasModel
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import load_model
+from tensorflow.keras.models import Model as kerasModel
 import numpy as np
 import pandas as pd
-import tensorflow as tf
 
 from ayniy.model.model import Model as oriModel
-
-# tensorflowの警告抑制
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "1"
-tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 
 def get_keras_data(
@@ -27,100 +18,6 @@ def get_keras_data(
     for c in categorical_features:
         X[c] = df[c]
     return X
-
-
-class CyclicLR(Callback):
-    def __init__(
-        self,
-        base_lr: float = 0.001,
-        max_lr: float = 0.006,
-        step_size: float = 2000.0,
-        mode: str = "triangular",
-        gamma: float = 1.0,
-        scale_fn: Any = None,
-        scale_mode: str = "cycle",
-    ):
-        super(CyclicLR, self).__init__()
-
-        self.base_lr = base_lr
-        self.max_lr = max_lr
-        self.step_size = step_size
-        self.mode = mode
-        self.gamma = gamma
-        if scale_fn is None:
-            if self.mode == "triangular":
-                self.scale_fn = lambda x: 1.0
-                self.scale_mode = "cycle"
-            elif self.mode == "triangular2":
-                self.scale_fn = lambda x: 1 / (2.0 ** (x - 1))
-                self.scale_mode = "cycle"
-            elif self.mode == "exp_range":
-                self.scale_fn = lambda x: gamma ** (x)
-                self.scale_mode = "iterations"
-        else:
-            self.scale_fn = scale_fn
-            self.scale_mode = scale_mode
-        self.clr_iterations = 0.0
-        self.trn_iterations = 0.0
-        self.history: Dict = {}
-
-        self._reset()
-
-    def _reset(
-        self,
-        new_base_lr: float = None,
-        new_max_lr: float = None,
-        new_step_size: float = None,
-    ) -> None:
-        """Resets cycle iterations.
-        Optional boundary/step size adjustment.
-        """
-        if new_base_lr is not None:
-            self.base_lr = new_base_lr
-        if new_max_lr is not None:
-            self.max_lr = new_max_lr
-        if new_step_size is not None:
-            self.step_size = new_step_size
-        self.clr_iterations = 0.0
-
-    def clr(self) -> float:
-        cycle = np.floor(1 + self.clr_iterations / (2 * self.step_size))
-        x = np.abs(self.clr_iterations / self.step_size - 2 * cycle + 1)
-        if self.scale_mode == "cycle":
-            return self.base_lr + (self.max_lr - self.base_lr) * np.maximum(
-                0, (1 - x)
-            ) * self.scale_fn(cycle)
-        else:
-            return self.base_lr + (self.max_lr - self.base_lr) * np.maximum(
-                0, (1 - x)
-            ) * self.scale_fn(self.clr_iterations)
-
-    def on_train_begin(self, logs: Dict = {}) -> None:
-        logs = logs or {}
-
-        if self.clr_iterations == 0:
-            K.set_value(self.model.optimizer.lr, self.base_lr)
-        else:
-            K.set_value(self.model.optimizer.lr, self.clr())
-
-    def on_batch_end(self, epoch: int, logs: Dict = None) -> None:
-        logs = logs or {}
-        self.trn_iterations += 1
-        self.clr_iterations += 1
-
-        self.history.setdefault("lr", []).append(K.get_value(self.model.optimizer.lr))
-        self.history.setdefault("iterations", []).append(self.trn_iterations)
-
-        for k, v in logs.items():
-            self.history.setdefault(k, []).append(v)
-
-        K.set_value(self.model.optimizer.lr, self.clr())
-
-
-def se_block(input: Any, channels: int, r: int = 8) -> Any:
-    x = Dense(channels // r, activation="relu")(input)
-    x = Dense(channels, activation="sigmoid")(x)
-    return Multiply()([input, x])
 
 
 class ModelNN(oriModel):
@@ -144,76 +41,59 @@ class ModelNN(oriModel):
         patience = self.params["patience"]
 
         # モデルの構築
+        model = keras.Sequential([
+            layers.Dense(64, activation='relu'),
+            layers.Dense(16, activation='relu'),
+            layers.Dense(1, activation='linear'),
+        ])
         inp_cats = []
         embs = []
         data = pd.concat([tr_x, va_x, te_x]).reset_index(drop=True)
 
         for c in self.categorical_features:  # type: ignore
-            inp_cat = Input(shape=[1], name=c)
+            inp_cat = layers.Input(shape=[1], name=c)
             inp_cats.append(inp_cat)
-            embs.append((Embedding(data[c].max() + 1, 4)(inp_cat)))
+            embs.append((layers.Embedding(data[c].max() + 1, 4)(inp_cat)))
 
-        cats = Flatten()(concatenate(embs))
-        cats = Dense(4, activation="linear")(cats)
-        cats = BatchNormalization()(cats)
-        cats = PReLU()(cats)
+        cats = layers.Flatten()(layers.concatenate(embs))
+        cats = layers.Dense(4, activation="linear")(cats)
 
-        inp_numerical = Input(shape=[len(numerical_features)], name="numerical")
-        nums = Dense(500, activation="linear")(inp_numerical)
-        nums = BatchNormalization()(nums)
-        nums = PReLU()(nums)
-        x = Dropout(dropout)(nums)
+        inp_numerical = layers.Input(shape=[len(numerical_features)], name="numerical")
+        nums = layers.Dense(500, activation="relu")(inp_numerical)
+        nums = layers.BatchNormalization()(nums)
+        x = layers.Dropout(dropout)(nums)
 
-        x = concatenate([nums, cats])
-        x = se_block(nums, 32)
-        x = BatchNormalization()(x)
-        x = Dropout(dropout / 2)(x)
-        x = Dense(250, activation="relu")(x)
-        x = Dense(125, activation="relu")(x)
-        x = Dense(64, activation="relu")(x)
-        x = Dense(40, activation="relu")(x)
-        out = Dense(31, activation="softmax", name="out1")(x)
+        x = layers.concatenate([nums, cats])
+        x = layers.Dense(40, activation="relu")(x)
+        out = layers.Dense(31, activation="linear", name="out1")(x)
 
         model = kerasModel(inputs=[inp_numerical], outputs=out)
-        model.compile(loss="sparse_categorical_crossentropy", optimizer="adam")
-        # print(model.summary())
-        n_train = len(tr_x)
-        batch_size_nn = 256
+        model.compile(
+            optimizer='adam',
+            loss='mse',
+            metrics=[keras.metrics.RootMeanSquaredError()]
+        )
+        batch_size = 256
 
         tr_x = get_keras_data(tr_x, numerical_features, self.categorical_features)
         va_x = get_keras_data(va_x, numerical_features, self.categorical_features)
 
-        clr_tri = CyclicLR(
-            base_lr=1e-5,
-            max_lr=1e-2,
-            step_size=n_train // batch_size_nn,
-            mode="triangular2",
-        )
-        ckpt = ModelCheckpoint(
-            f"../output/model/model_{self.run_fold_name}.hdf5",
-            save_best_only=True,
-            monitor="val_loss",
-            mode="min",
-        )
         if validation:
-            early_stopping = EarlyStopping(
-                monitor="val_loss",
+            early_stopping = keras.callbacks.EarlyStopping(
                 patience=patience,
-                verbose=1,
+                min_delta=0.001,
                 restore_best_weights=True,
             )
             model.fit(
-                tr_x,
-                tr_y,
-                epochs=nb_epoch,
-                batch_size=batch_size_nn,
-                verbose=2,
+                tr_x, tr_y,
                 validation_data=(va_x, va_y),
-                callbacks=[ckpt, clr_tri, early_stopping],
+                batch_size=batch_size,
+                epochs=nb_epoch,
+                callbacks=[early_stopping],
             )
         else:
             model.fit(
-                tr_x, tr_y, nb_epoch=nb_epoch, batch_size=batch_size_nn, verbose=2
+                tr_x, tr_y, batch_size=batch_size, nb_epoch=nb_epoch
             )
         model.load_weights(f"../output/model/model_{self.run_fold_name}.hdf5")  # type: ignore
 
