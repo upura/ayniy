f8b75c709717f6b80a070a382b6f5b92606c215d
upura <upura0@gmail.com>
2021-01-06 03:10:00 +0900

    RandomForest

--- ayniy/model/__init__.py
+++ ayniy/model/__init__.py
@@ -4,6 +4,7 @@ from ayniy.model.model_cat import ModelCatClassifier, ModelCatRegressor
 from ayniy.model.model_lgbm import ModelFocalLGBM, ModelLGBM, ModelOptunaLGBM
 from ayniy.model.model_knn import ModelKNN
 from ayniy.model.model_nn import ModelNN
+from ayniy.model.model_rf import ModelRFClassifier, ModelRFRegressor
 from ayniy.model.model_ridge import ModelRIDGE
 from ayniy.model.model_svm import ModelSVM
 from ayniy.model.model_xgb import ModelXGB
@@ -16,6 +17,8 @@ __all__: List[Any] = [
     ModelKNN,
     ModelNN,
     ModelOptunaLGBM,
+    ModelRFClassifier,
+    ModelRFRegressor,
     ModelRIDGE,
     ModelSVM,
     ModelXGB,
